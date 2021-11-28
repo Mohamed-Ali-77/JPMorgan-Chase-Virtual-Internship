53ef6882316d80f0d4468f029e6daf0548ce6cd6
Mohamed Ali <85204433+Mohamed-Ali-77@users.noreply.github.com>
2021-11-29 00:35:34 +0200

    Ubdated Code

---
+++ JPMC-tech-task-2-PY3/src/App.tsx
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import DataStreamer, { ServerRespond } from './DataStreamer';
+import Graph from './Graph';
+import './App.css';
+
+/**
+ * State declaration for <App />
+ */
+interface IState {
+  data: ServerRespond[],
+  showGraph: boolean, //First you’ll need to add the `showGraph` property in the IState interface 
+                      //It should be of the type` boolean`
+}
+
+/**
+ * The parent element of the react app.
+ * It renders title, button and Graph react element.
+ */
+class App extends Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      // data saves the server responds.
+      // We use this state to parse data down to the child element (Graph) as element property
+      data: [],
+      showGraph: false, //Then,defined that the initial state of the App not to show the graph yet
+                        
+    };
+  }
+
+  /**
+   * Render Graph react component with state.data parse as property data
+   */
+  renderGraph() {
+    if(this.state.showGraph){             //then,I added a condition to only render the graph when the state’s `showGraph` property of the App’s state is `true`
+      return (<Graph data={this.state.data}/>)
+    }
+  }
+
+  /**
+   * Get new data from server and update the state with the new data
+   */
+  getDataFromServer() {   //Finally, modified the `getDataFromServer`method to contact the 
+                          //server and get data from it continuously instead of just getting data from it 
+                          //once every time clicking the button.
+    let x =0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+      // Update the state by creating a new array of data that consists of
+      // Previous data in the state and the new data from server
+        this.setState({
+           data: serverResponds,
+           showGraph: true,
+        });
+      });
+      x++;
+      if(x>10000){
+        clearInterval(interval);
+      }
+    }, 100);
+  }
+
+  /**
+   * Render the App react component
+   */
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Bank & Merge Co Task 2
+        </header>
+        <div className="App-content">
+          <button className="btn btn-primary Stream-button"
+            // when button is click, our react app tries to request
+            // new data from the server.
+            // As part of your task, update the getDataFromServer() function
+            // to keep requesting the data every 100ms until the app is closed
+            // or the server does not return anymore data.
+            onClick={() => {this.getDataFromServer()}}>
+            Start Streaming Data
+          </button>
+          <div className="Graph">
+            {this.renderGraph()}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
